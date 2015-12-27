d1986f175c579f7bba1845c18e22fe80eebaeb1a
Nikush Dalia <nikushdalia@gmail.com>
2015-12-26 20:53:41 -0500

    Parrot app as it was first submitted

---
+++ Parrot.py
@@ -0,0 +1,186 @@
+"""
+Developer: Nikush Dalia
+Skill: Parrot
+Description: Repeat a phrase that the user gives it.
+"""
+
+from __future__ import print_function
+
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "ParrotIntent":
+        return set_parrot_in_session(intent, session)
+    elif intent_name == "SayParrotIntent":
+        return get_parrot_from_session(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+# --------------- Functions that control the skill's behavior ------------------
+
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome to Parrot. " \
+                    "Please say your parrot phrase."
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Again, please say your parrot phrase."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def set_parrot_in_session(intent, session):
+    """ Sets the color in the session and prepares the speech to reply to the
+    user.
+    """
+
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+
+    if 'Parrot' in intent['slots']:
+        parrot_phrase = intent['slots']['Parrot']['value']
+        session_attributes = create_parrot_phrase_attributes(parrot_phrase)
+        speech_output = "Your phrase is: " + \
+                        parrot_phrase + \
+                        ". You can make me repeat this phrase by asking, " \
+                        "what's my phrase?"
+        reprompt_text = "You can make me repeat this phrase by asking, " \
+                        "what's my phrase?"
+    else:
+        speech_output = "I'm not sure what your phrase was. " \
+                        "Please try again."
+        reprompt_text = "I'm not sure what your phrase. " \
+                        "You can tell me your phrase by saying, " \
+                        "set my phrase to, and then your phrase."
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def create_parrot_phrase_attributes(parrot_phrase):
+    return {"Parrot": parrot_phrase}
+
+
+def get_parrot_from_session(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    if "Parrot" in session.get('attributes', {}):
+        parrot_phrase = session['attributes']['Parrot']
+        speech_output = "Your phrase is: " + parrot_phrase + \
+                        ". Goodbye."
+        should_end_session = True
+    else:
+        speech_output = "You haven't set a phrase. " \
+                        "To set one, you can say set my phrase to, and then your desired phrase."
+        should_end_session = False
+
+    # Setting reprompt_text to None signifies that we do not want to reprompt
+    # the user. If the user does not respond or says something that is not
+    # understood, the session will end.
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
